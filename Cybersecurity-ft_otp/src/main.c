e7ab833682da0ac88bd6d5a829b780bfc1b9fa38
0xEr3b0s <78426587+0xEr3b0s@users.noreply.github.com>
2026-06-19 18:00:58 +0200

    [feature] working on encrypt key

--- Cybersecurity-ft_otp/src/main.c
+++ Cybersecurity-ft_otp/src/main.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "ft_otp.h"
 
 int main(int ac, char **av) {
 	params_t params;
+	memset(&params, 0, sizeof(params));
 
-	if (ac != 3) error("Should get atleast one option (-g/-k) and it's related argument.");
+	if (ac != 3)
+		error("Should get one option (-g/-k) and its related argument.");
+
 	parse_option(&params, av);
 
-	if (params.g_flag)
+	if (params.g_flag) {
 		check_key(&params);
+		store_key(&params);
+		free(params.key);
+	} else if (params.k_flag) {
+		load_key(&params);
+		printf("%06d\n", generate_otp(&params));
+		free(params.key);
+	}
 
-	decode_hex(&params);
-	if (DEBUG_FLAG)
-		debug_hexdump("key", params.decoded_key, params.decoded_key_size);
-
-	encode_counter(&params);
-	if (DEBUG_FLAG)
-		debug_hexdump("counter", params.counter, 8);
-
-	free(params.key);
-	free(params.decoded_key);
 	return (0);
 }
--- Cybersecurity-ft_otp/src/parsing.c
+++ Cybersecurity-ft_otp/src/parsing.c
@@ -8,8 +8,8 @@ void parse_option(params_t *params, char **av) {
 	char *option = av[1];
 	char *key_file = av[2];
 
-	memset(params, 0, sizeof(params_t));
-	if (option[0] != '-') error("first argument should be the option (-g/-k)");
+	if (option[0] != '-')
+		error("first argument should be the option (-g/-k)");
 
 	if (option[1] == 'g')
 		params->g_flag = 1;
@@ -17,6 +17,7 @@ void parse_option(params_t *params, char **av) {
 		params->k_flag = 1;
 	else
 		error("unrecognized option (-g/-k)");
+
 	params->filename = key_file;
 }
 
@@ -25,9 +26,10 @@ static char *concat(const char *s1, const char *s2) {
 	const size_t len2 = strlen(s2);
 	char *result = malloc(len1 + len2 + 1);
 
-	if (!result) error("malloc failed in concat");
-	if (len1 > 0) memcpy(result, s1, len1 + 1);
-	result[len1 + 1] = '\0'; // no impact just to disable warning
+	if (!result)
+		error("malloc failed in concat");
+	if (len1 > 0)
+		memcpy(result, s1, len1);
 	memcpy(result + len1, s2, len2 + 1);
 	return result;
 }
@@ -39,7 +41,8 @@ void check_key(params_t *params) {
 	size_t count_char;
 
 	fptr = fopen(params->filename, "r");
-	if (!fptr) error("could not open the file");
+	if (!fptr)
+		error("could not open the file");
 	while (fgets(data, 64, fptr)) {
 		char *tmp = rebuild_str;
 		rebuild_str = concat(tmp, data);
@@ -47,7 +50,8 @@ void check_key(params_t *params) {
 	}
 	fclose(fptr);
 
-	if (!rebuild_str) error("the key file is empty");
+	if (!rebuild_str)
+		error("the key file is empty");
 
 	count_char = strlen(rebuild_str);
 	while (count_char > 0 && (rebuild_str[count_char - 1] == '\n' ||
@@ -56,7 +60,7 @@ void check_key(params_t *params) {
 
 	if (count_char < 64 || (count_char % 2) != 0) {
 		char tmp[128];
-		sprintf(tmp, "key needs minimum 64 character (%zu) and be pair",
+		sprintf(tmp, "key needs minimum 64 character (%zu) and be even",
 				count_char);
 		free(rebuild_str);
 		error(tmp);
@@ -68,10 +72,13 @@ void check_key(params_t *params) {
 		}
 	}
 
-	params->key = (char *)malloc(count_char * sizeof(char));
+	params->key = malloc(count_char);
+	if (!params->key) {
+		free(rebuild_str);
+		error("malloc failed in check_key");
+	}
 	memcpy(params->key, rebuild_str, count_char);
 	params->key_size = count_char;
-	params->decoded_key_size = count_char / 2;
 
 	free(rebuild_str);
 }
