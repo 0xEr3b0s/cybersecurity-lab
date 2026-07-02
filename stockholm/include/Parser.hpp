1c6dd2ce2e72442ecf76b002371b8842b710f33b
0xEr3b0s <78426587+0xEr3b0s@users.noreply.github.com>
2026-07-02 22:17:48 +0200

    [feature] parsed arguments

--- stockholm/include/Parser.hpp
+++ stockholm/include/Parser.hpp
@@ -2,20 +2,23 @@
 
 #include <string>
 
-#define NORMAL_MOD_CODE 1
-#define REVERSE_MOF_CODE 2
-#define SILENT_MOD_CODE 3
+#define OPT_SILENT (1 << 0)
+#define OPT_REVERSE (1 << 1)
+#define OPT_VERSION (1 << 2)
+#define OPT_HELP (1 << 3)
+
+#define PROGRAM_VERSION "0.0.1"
 
 class Parser {
 	private:
-		int option_code;
-		std::string key;
+		int option_field;
+		char *key;
 
 		static char *getCmdOption(char ** begin, char ** end, const std::string & option);
 		static bool cmdOptionExists(char** begin, char** end, const std::string& option);
 
 	public:
 		Parser *parse(int ac, char **av);
-		int getOptionCode(void) const;
+		int getOptionfield(void) const;
 		std::string getKey(void) const;
 };
--- stockholm/src/main.cpp
+++ stockholm/src/main.cpp
@@ -2,12 +2,12 @@
 #include "Parser.hpp"
 
 int main(int ac, char **av) {
-	if (ac < 2 || ac > 3) {
+	if (ac < 2) {
 		std::cerr << "AssertionError: need atleast 1 argument and max 2 (option - key)." << std::endl;
 		exit (1);
 	}
 	std::cout << "Hello stockholm" << std::endl;
 	Parser *p = Parser().parse(ac, av);
-	std::cout << p->getKey() << " :: " << p->getOptionCode() << std::endl;
+	std::cout << p->getKey() << " :: " << p->getOptionfield() << std::endl;
 	return 0;
 }
