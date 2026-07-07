927481876205e5b92f8cd38c3c6c6dcc711e7bd6
0xEr3b0s <78426587+0xEr3b0s@users.noreply.github.com>
2026-07-07 21:31:09 +0200

    [feature] finished encrypt data

--- stockholm/include/Cipher.hpp
+++ stockholm/include/Cipher.hpp
@@ -5,6 +5,9 @@
 class Cipher {
 	private:
 		const std::string new_ext = ".ft";
+		void encryptFile(std::filesystem::directory_entry &path, std::string &key);
+		void decryptFile(std::filesystem::directory_entry &path, std::string &key);
+
 	public:
-		Cipher(std::vector<std::filesystem::directory_entry> &final_paths_vector, std::string &key);
+		Cipher(std::vector<std::filesystem::directory_entry> &final_paths_vector, std::string &key, int &option_field);
 };
