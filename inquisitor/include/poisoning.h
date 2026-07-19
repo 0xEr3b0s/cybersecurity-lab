a4e406fe72e111cb8d8d0f391e90e7b0a279786e
0xEr3b0s <78426587+0xEr3b0s@users.noreply.github.com>
2026-07-19 10:00:45 +0200

    [feature] structures

---
+++ inquisitor/include/poisoning.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct s_eth_header {
+
+} s_eth_header;
+
+typedef struct s_arp_msg {
+
+} t_arp_msg;
+
+typedef struct s_arp_frame {
+
+} __attribute__((__packed__)) t_arp_frame;
