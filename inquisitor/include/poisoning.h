6537c953eac23ca37eb9b8d38ce3411bcda8538f
0xEr3b0s <78426587+0xEr3b0s@users.noreply.github.com>
2026-07-28 14:53:31 +0200

    [feature] finished the 3 structures

--- inquisitor/include/poisoning.h
+++ inquisitor/include/poisoning.h
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#ifndef POISONNING_H
+#define POISONNING_H
+
 typedef struct s_eth_header {
 	unsigned char mac_dst[6];
 	unsigned char mac_src[6];
 	unsigned short ether_type;
-} __attribute__((__packed__)) s_eth_header;
+} __attribute__((__packed__)) t_eth_header;
 
 typedef struct s_arp_msg {
 	unsigned short hardware_type;
 	unsigned short protocol_type;
+	unsigned short opcde;
 	unsigned char hardware_addr_len;
 	unsigned char protocol_addr_len;
+	unsigned char sender_mac[6];
+	unsigned char sender_ip[2];
+	unsigned char target_mac[6];
+	unsigned char target_ip[6];
 } t_arp_msg;
 
 typedef struct s_arp_frame {
-
+	t_eth_header eth;
+	t_arp_msg arp;
 } __attribute__((__packed__)) t_arp_frame;
+
+#endif
