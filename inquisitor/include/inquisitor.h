8eab3fb7db5a7dc8d5f6e5d8f0e83efeffa37f2d
0xEr3b0s <78426587+0xEr3b0s@users.noreply.github.com>
2026-07-28 22:02:46 +0200

    [feature] almost finished poisoning part

--- inquisitor/include/inquisitor.h
+++ inquisitor/include/inquisitor.h
@@ -2,13 +2,15 @@
 #define INQUISITOR_H
 
 typedef struct s_config {
-	char *ip_src;
-	char *mac_src;
-	char *ip_dest;
-	char *mac_dest;
+	char *spoof_ip;
+	char *spoof_mac;
 
-	char *ip_local;
-	char *mac_local;
+	char *target_ip;
+	char *target_mac;
+
+	char *local_ip;
+	char *local_mac;
+
 	int ifindex;
 } t_config;
 
--- inquisitor/include/poisoning.h
+++ inquisitor/include/poisoning.h
@@ -1,26 +1,29 @@
-#include <stdio.h>
-#include <stdint.h>
-
 #ifndef POISONNING_H
 #define POISONNING_H
 
+#include <stdio.h>
+#include <stdint.h>
+
 typedef struct s_eth_header {
-	unsigned char mac_dst[6];
-	unsigned char mac_src[6];
+	unsigned char dst_mac[6];
+	unsigned char src_mac[6];
 	unsigned short ether_type;
-} __attribute__((__packed__)) t_eth_header;
+} __attribute__((packed)) t_eth_header;
 
 typedef struct s_arp_msg {
 	unsigned short hardware_type;
 	unsigned short protocol_type;
-	unsigned short opcde;
+	unsigned short opcode;
+
 	unsigned char hardware_addr_len;
 	unsigned char protocol_addr_len;
+
 	unsigned char sender_mac[6];
 	unsigned char sender_ip[4];
+
 	unsigned char target_mac[6];
 	unsigned char target_ip[4];
-} __attribute__((__packed__)) t_arp_msg;
+} __attribute__((packed)) t_arp_msg;
 
 typedef struct s_arp_frame {
 	t_eth_header eth;
