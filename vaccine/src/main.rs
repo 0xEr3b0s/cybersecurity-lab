1b35c87d370b59cfe7006dcb41061e35c82ade82
0xEr3b0s <78426587+0xEr3b0s@users.noreply.github.com>
2026-08-12 16:20:33 +0200

    [corr] put back the basic main file i had

--- vaccine/src/main.rs
+++ vaccine/src/main.rs
@@ -6,10 +6,13 @@ fn error(msg: String, code: i32) {
     process::exit(code);
 }
 
+/// Arg count should be between 2 and 6 because can go like:
+/// ["./vaccine", "-X", "POST", "-o", "res.txt", "http://h/?id=1"],
+/// and the program name is always here
 fn check_args() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        error("Not enought arguments".to_owned(), 1);
+    if args.len() < 2 && args.len() > 6 {
+        error("Bad arguments given".to_owned(), 1);
     }
     args
 }
