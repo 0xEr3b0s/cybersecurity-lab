3c53492f434b84f7dd0d2e15998067342a4ae0fd
0xEr3b0s <78426587+0xEr3b0s@users.noreply.github.com>
2026-08-12 13:12:14 +0200

    [feature] starting parsing arguments

--- vaccine/src/main.rs
+++ vaccine/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::process;
+
+fn error(msg: String, code: i32) {
+    eprintln!("[ERROR] {msg}", msg=msg);
+    process::exit(code);
+}
+
+fn check_args() -> Vec<String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        error("Not enought arguments".to_owned(), 1);
+    }
+    args
+}
+
 fn main() {
     println!("Hello, world!");
+    let args = check_args();
+    println!("Passed args check");
+    print!("{:?}", args);
 }
